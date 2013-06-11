582ec840109f7415a4799221a8ddf80aea88bc4b
Ian T Richards <ianr@panix.com>
2013-06-10 22:04:26 -0400

    Checkpoint ... playing around w/csweb and tk

--- gui/upnp-console.tcl
+++ gui/upnp-console.tcl
@@ -5,7 +5,7 @@ package require json
 package require http
 
 namespace eval state {
-	variable volume
+	variable volume 0 mute 0
 }
 
 set URL "http://localhost:8080"
@@ -31,9 +31,23 @@ proc get_volume {} {
 	http::geturl "$::CONTROL?method=get-volume" -command on_get_volume
 }
 
+proc on_mute_button {} {
+	puts "set mute state $state::mute"
+}
+
 wm title . "UPnP Console"
-pack [frame .top_level]
-pack [label .top_level.volume_label -text "Volume"]
-pack [scale .top_level.volume_scale -command on_volume_scale -from 0 -to 100 -showvalue 1 -variable state::volume]
+pack [ttk::frame .top_level]
+pack [ttk::notebook .top_level.notebook]
+pack [ttk::frame .top_level.notebook.control]
+pack [ttk::frame .top_level.notebook.content]
+.top_level.notebook add .top_level.notebook.control -text "Control"
+.top_level.notebook add .top_level.notebook.content -text "Content"
+
+set vf .top_level.notebook.control.volume
+pack [ttk::labelframe $vf -text "Volume"]
+pack [ttk::label $vf.volume_label -text "Volume"]
+pack [scale $vf.volume_scale -command on_volume_scale -from 0 -to 100 -variable state::volume -showvalue 1 -orient horizontal]
+pack [ttk::label $vf.mute_label -text "Mute"]
+pack [ttk::checkbutton $vf.mute -command on_mute_button -variable state::mute]
 get_volume
 
