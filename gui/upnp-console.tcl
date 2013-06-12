b3c8284fb9fb277f1961fd9ee14c06d3d61733e7
Ian T Richards <ianr@panix.com>
2013-06-11 22:38:49 -0400

    Adding sample select box for genres

--- gui/upnp-console.tcl
+++ gui/upnp-console.tcl
@@ -10,6 +10,7 @@ namespace eval state {
 
 set URL "http://localhost:8080"
 set CONTROL "$URL/control"
+set BROWSE "$URL/browse"
 
 proc on_set_volume {token} {
 	upvar $token state
@@ -35,19 +36,39 @@ proc on_mute_button {} {
 	puts "set mute state $state::mute"
 }
 
+proc on_get_genres {token} {
+	upvar $token state
+	set msg [dict get [json::json2dict $state(body)] Value ]
+	foreach item $msg {
+		set title [dict get $item Title]
+		lappend genres $title
+	}
+	.top_level.notebook.content.genre configure -values $genres
+}
+
+proc get_genres {} {
+	http::geturl "$::BROWSE?method=get-all-genres" -command on_get_genres
+}
+
 wm title . "UPnP Console"
-pack [ttk::frame .top_level]
+pack [frame .top_level]
 pack [ttk::notebook .top_level.notebook]
-pack [ttk::frame .top_level.notebook.control]
-pack [ttk::frame .top_level.notebook.content]
+pack [frame .top_level.notebook.control]
+pack [frame .top_level.notebook.content]
 .top_level.notebook add .top_level.notebook.control -text "Control"
 .top_level.notebook add .top_level.notebook.content -text "Content"
 
 set vf .top_level.notebook.control.volume
-pack [ttk::labelframe $vf -text "Volume"]
-pack [ttk::label $vf.volume_label -text "Volume"]
+pack [labelframe $vf -text "Volume"]
+pack [label $vf.volume_label -text "Volume"]
 pack [scale $vf.volume_scale -command on_volume_scale -from 0 -to 100 -variable state::volume -showvalue 1 -orient horizontal]
-pack [ttk::label $vf.mute_label -text "Mute"]
-pack [ttk::checkbutton $vf.mute -command on_mute_button -variable state::mute]
-get_volume
+pack [label $vf.mute_label -text "Mute"]
+pack [checkbutton $vf.mute -command on_mute_button -variable state::mute]
 
+set vf .top_level.notebook.content
+set genre_box [ttk::combobox $vf.genre -text "Genre"]
+$genre_box configure -values {"1" "2"}
+pack $genre_box
+
+get_volume
+get_genres
