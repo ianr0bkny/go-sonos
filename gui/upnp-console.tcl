98f68fdeb017252bce3b68b51a6554351729f47d
Ian T Richards <ianr@panix.com>
2013-06-12 22:05:52 -0400

    Checkpoint

--- gui/upnp-console.tcl
+++ gui/upnp-console.tcl
@@ -51,24 +51,27 @@ proc get_genres {} {
 }
 
 wm title . "UPnP Console"
-pack [frame .top_level]
-pack [ttk::notebook .top_level.notebook]
-pack [frame .top_level.notebook.control]
-pack [frame .top_level.notebook.content]
+wm minsize . 600 600
+pack [frame .top_level] -fill both
+pack [ttk::notebook .top_level.notebook] -fill both
+pack [frame .top_level.notebook.control] -fill both
+pack [frame .top_level.notebook.content] -fill both
 .top_level.notebook add .top_level.notebook.control -text "Control"
 .top_level.notebook add .top_level.notebook.content -text "Content"
 
 set vf .top_level.notebook.control.volume
-pack [labelframe $vf -text "Volume"]
+pack [labelframe $vf -text "Volume"] -fill both
 pack [label $vf.volume_label -text "Volume"]
 pack [scale $vf.volume_scale -command on_volume_scale -from 0 -to 100 -variable state::volume -showvalue 1 -orient horizontal]
 pack [label $vf.mute_label -text "Mute"]
 pack [checkbutton $vf.mute -command on_mute_button -variable state::mute]
 
 set vf .top_level.notebook.content
-set genre_box [ttk::combobox $vf.genre -text "Genre"]
-$genre_box configure -values {"1" "2"}
-pack $genre_box
+pack [ttk::combobox $vf.genre -text "Genre"]
+pack [ttk::combobox $vf.artist -text "Artist"]
+pack [ttk::combobox $vf.album -text "Album"]
+pack [ttk::combobox $vf.track -text "Trackk"]
 
 get_volume
 get_genres
+
