e63b5904b342cdaa5bdd6e06c05fcbe25c74a77e
Ian T Richards <ianr@panix.com>
2015-07-21 22:25:34 -0400

    adding comments to example code

--- examples/discovery/discovery.go
+++ examples/discovery/discovery.go
@@ -38,11 +38,22 @@ import (
 // MusicServices API.
 func main() {
 	log.Print("go-sonos example discovery\n")
+
 	mgr := ssdp.MakeManager()
+
+	// Discover()
+	//  eth0 := Network device to query for UPnP devices
+	// 11209 := Free local port for discovery replies
+	// false := Do not subscribe for asynchronous updates
 	mgr.Discover("eth0", "11209", false)
+
+	// SericeQueryTerms
+	// A map of service keys to minimum required version
 	qry := ssdp.ServiceQueryTerms{
 		ssdp.ServiceKey("schemas-upnp-org-MusicServices"): -1,
 	}
+
+	// Look for the service keys in qry in the database of discovered devices
 	result := mgr.QueryServices(qry)
 	if dev_list, has := result["schemas-upnp-org-MusicServices"]; has {
 		for _, dev := range dev_list {
